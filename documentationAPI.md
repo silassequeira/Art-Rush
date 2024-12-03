4c9a9e7588f15747313f0f586060f6ad813c1188
silassequeira <silassequeiraa@gmail.com>
2024-12-03 19:04:34 +0000

    WE HAVE ARTWORKS!

--- documentationAPI.md
+++ documentationAPI.md
@@ -6,5 +6,7 @@
 At this time, we do not require API users to register or obtain an API key to use the service. Please limit request rate to 80 requests per second.
 
 
+objectName 
 
+title
 
--- server.js
+++ server.js
@@ -6,54 +6,38 @@ const path = require('path')
 const server = express()
 
 async function fetchCats() {
-    const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/[isHighlight]?limit=80')
+    const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11&limit=1000')
     const data = await response.json()
+    const objectIDs = data.objectIDs.slice(0, 90) // Limit to 80 objects
 
-    fs.writeFileSync('catData.json', JSON.stringify(data, null, 2))
+    const records = []
 
-    console.log("Fetched cat data")
+    for (const objectID of objectIDs) {
+        const objectResponse = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+        const objectData = await objectResponse.json()
+
+        if (objectData.objectName === 'Painting') {
+            const record = {
+                primaryImage: objectData.primaryImage,
+                primaryImageSmall: objectData.primaryImageSmall,
+                isHighlight: objectData.isHighlight,
+                objectName: objectData.objectName,
+                title: objectData.title,
+                artistDisplayName: objectData.artistDisplayName,
+                artistDisplayBio: objectData.artistDisplayBio,
+                artistNationality: objectData.artistNationality,
+                objectDate: objectData.objectDate,
+                medium: objectData.medium,
+            }
+
+            records.push(record)
+        }
+    }
+
+    fs.writeFileSync('collection.json', JSON.stringify(records, null, 2))
+
+    console.log("Fetched all data")
 }
-
-function getAllCatsIds() {
-    const fileData = fs.readFileSync('catData.json', 'utf8');
-    const allCats = JSON.parse(fileData)
-
-    return allCats
-}
-
-server.use(express.static(path.join(__dirname, 'public')));
-
-server.all('/cat-data', (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = 20;
-    const offset = (page - 1) * limit;
-
-    const catData = getAllCatsIds(limit, offset);
-    let html = '';
-
-    catData.forEach(cat => {
-        html += `
-            <img 
-                src="https://cataas.com/cat/${cat._id}" 
-                alt="Cat Image" 
-                style="width: 100%; height: auto; display: block; margin-bottom: 1em;" 
-                loading="lazy"
-            />
-        `;
-    });
-
-    const nextPage = catData.length === limit ? page + 1 : null;
-
-    res.json({
-        html,
-        nextPage
-    });
-});
-
-
-server.all('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000')
