942d9d700953af11ec297ac238a7779f802961a3
silassequeira <silassequeiraa@gmail.com>
2024-12-10 13:33:21 +0000

    Merged files from fetchApi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Hello from './components/Hello' 
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="Hello">
+   <Hello />
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/componentTest.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+let Hello = (props) => {
+  return (
+    <div>
+      <h1>Hello, {Mike} World!</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+};
+
+export default Hello;
